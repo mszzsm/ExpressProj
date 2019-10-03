5883e8b3d663441997870a91161683613dbcc370
Michał Sztefanica <mikeshtefanitsa@gmail.com>
2019-10-03 09:19:38 +0300

    express  installed

---
+++ hello.js
@@ -0,0 +1,7 @@
+const http = require('http');
+
+http.createServer((request, response) => {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Hello, Word!\n');
+    response.end();
+}).listen(1337);
